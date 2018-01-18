e3f536fb47e3caefb9149fd3199be355900b4a85
cloughurd <benkclough@gmail.com>
2018-01-18 11:32:13 -0700

    adding new files

---
+++ Dictionary.java
@@ -0,0 +1,10 @@
+package spell;
+
+public class Dictionary implements ITrie{
+
+    private INode root = new INode;
+    private int uniqueWordCount = 0;
+    private int wordCount = 0;
+    private int nodeCount = 0;
+
+}
---
+++ INode.java
@@ -0,0 +1,8 @@
+#package spell;
+
+public class INode{
+
+    public INode[] nodes = new INode[26];
+    public int count = 0;
+
+}
